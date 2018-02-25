6a2736117d06beb60a8685fcb0f565d11c1cb99c
Unknown <jason.gs.lum@gmail.com>
2018-02-24 17:34:09 -0800

    Initial Unity Project Commit
    Have basic platforms and player

---
+++ Assets/PlatformSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+	public float xPosition;
+	public Platform[] PlatformPrefabs;
+	public Platform[] Platforms;
+	public float SpawnIncrement = 13.25f;
+	public float SpawnTime;
+	private float SpawnPosition;
+	private float Timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		int idx = Random.Range (0, PlatformPrefabs.Length-1);
+		SpawnPosition = SpawnIncrement;
+		Platform p1 = Instantiate (PlatformPrefabs[idx], new Vector3 (xPosition, PlatformPrefabs[idx].transform.position.y, 0), new Quaternion());
+		idx = Random.Range (0, PlatformPrefabs.Length);
+		Platform p2 = Instantiate (PlatformPrefabs[idx], new Vector3 (xPosition, PlatformPrefabs[idx].transform.position.y, SpawnPosition), new Quaternion());
+		Platforms = new Platform[] {
+			p1,
+			p2
+		};
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Timer += Time.deltaTime;
+		if (Timer >= SpawnTime) {
+			Timer -= SpawnTime;
+			AddNewPlatform ();
+		}
+	}
+
+	private void AddNewPlatform()
+	{
+		SpawnPosition += SpawnIncrement;
+		Destroy (Platforms [0].gameObject);
+		Platforms [0] = Platforms [1];
+		int idx = Random.Range (0, PlatformPrefabs.Length);
+		Platforms[1] = Instantiate (PlatformPrefabs[idx], new Vector3 (xPosition, PlatformPrefabs[idx].transform.position.y, SpawnPosition), new Quaternion());
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public Camera MainCamera;
+	public float Speed;
+	public Rigidbody RBD;
+	public float JumpHeight = 400;
+
+	// Use this for initialization
+	void Start () 
+	{
+		RBD = GetComponent<Rigidbody> ();
+		MainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		RegisterInput ();
+		MainCamera.transform.position = new Vector3 (transform.position.x, transform.position.y + 8, transform.position.z - 14);
+
+		MainCamera.transform.rotation = Quaternion.AngleAxis (24, transform.right);
+	}
+
+	private void RegisterInput ()
+	{
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0, 0, Speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0, 0, Speed * -Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (Speed * -Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Speed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && RBD.velocity.y == 0f && transform.position.y == 1.25) {
+			RBD.AddForce (0, JumpHeight, 0);
+		}
+	}
+}
